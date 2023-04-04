b3fb97a9d5bd0657636e318082bc97ac14e15677
Eliseo Arévalo <eliseoarevalo29@gmail.com>
2023-04-04 08:37:01 -0600

    [:package: add dependencies] RRD config

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
-
-import './App.css'
+import { useState } from "react"
+import {Routes,Route} from "react-router-dom"
+import "./App.css"
 
 function App() {
 
   return (
     <div className="App">
+      <Routes>
+        
+      </Routes>
     </div>
   )
 }
